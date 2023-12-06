4d46b2b00f3e31086af190951ade8a0891aae124
btwonion <btwonion@gmx.de>
2023-12-06 17:39:45 +0100

    add day06

---
+++ src/main/kotlin/dev/nyon/aoc/Day06.kt
@@ -0,0 +1,42 @@
+package dev.nyon.aoc
+
+/**
+ * @author btwonion
+ * @since 06/12/2023
+ */
+fun main() = day(6) {
+    test1Expected = 288
+    test2Expected = 71503
+
+    fun possibleWinCounts(time: Long, record: Long): Int {
+        var couldWin = 0
+        (0..time).forEach {
+            if (it * (time - it) > record) couldWin++
+        }
+        return couldWin
+    }
+
+    part1 {
+        val times = inputLines.first().drop(11).split(' ').filterNot { it.isEmpty() }.map { it.toInt() }
+        val records = inputLines[1].drop(11).split(' ').filterNot { it.isEmpty() }.map { it.toInt() }
+        // List of time - distance
+        val games = times.mapIndexed { index, time ->
+            time to records[index]
+        }
+
+        var margin = 0
+        games.forEach { (time, record) ->
+            val possibleWinCounts = possibleWinCounts(time.toLong(), record.toLong())
+            if (margin == 0) margin = possibleWinCounts
+            else margin *= possibleWinCounts
+        }
+
+        margin
+    }
+
+    part2 {
+        val (time, record) = inputLines.map { it.drop(11).replace(" ", "").toLong() }
+
+        possibleWinCounts(time, record)
+    }
+}
