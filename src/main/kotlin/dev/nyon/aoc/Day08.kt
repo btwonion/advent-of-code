ac5d560dfe31487eaad4c70da3b54a114c15fafd
btwonion <btwonion@gmx.de>
2023-12-08 16:29:20 +0100

    finish day08

--- src/main/kotlin/dev/nyon/aoc/Day08.kt
+++ src/main/kotlin/dev/nyon/aoc/Day08.kt
@@ -5,30 +5,45 @@ package dev.nyon.aoc
  * @since 08/12/2023
  */
 fun main() = day(8) {
-    test1Expected = 6
+    test1Expected = 2
+    test2Expected = 6
+
+    fun gcd(x: Long, y: Long): Long {
+        var a = x
+        var b = y
+        while (b != 0L) a = b.also { b = a.mod(b) }
+        return a
+    }
+
+    fun lcm(x: Long, y: Long): Long = x / gcd(x, y) * y
 
     fun String.parseWays(): Pair<String, String> {
         val (left, right) = drop(7).dropLast(1).split(", ")
         return left to right
     }
 
-    part1 {
-        val steps = inputLines.first()
+    val instructions = inputLines.first()
+    val directions = inputLines.drop(2).associate { it.take(3) to it.parseWays() }
 
-        var currentStep = inputLines.first { it.startsWith("AAA") }.parseWays()
-        var stepsNeeded = 0
-        var finished = false
-        while (!finished) {
-            steps.forEach { nextStep ->
-                if (finished) return@forEach
-                val nextPlace = inputLines.first { it.startsWith(if (nextStep == 'L') currentStep.first else currentStep.second) }
-                val nextWay = nextPlace.parseWays()
-                stepsNeeded++
-                if (nextPlace.startsWith("ZZZ")) finished = true
-                currentStep = nextWay
-            }
+    fun step(start: String) = instructions.fold(start) { loc, char ->
+        when (char) {
+            'L' -> directions[loc]!!.first
+            'R' -> directions[loc]!!.second
+            else -> throw IllegalStateException()
         }
+    }
 
-        stepsNeeded
+    part1 {
+        instructions.length * generateSequence("AAA", ::step).indexOf("ZZZ")
+    }
+
+    // Credits to: https://github.com/ephemient/aoc2023/
+    // Couldn't figure it out myself.
+    part2 {
+        val instructionsNeeded = directions.keys.filter { it.endsWith('A') }.fold(1L) { count, start ->
+            val index = generateSequence(start, ::step).withIndex().first { (_, end) -> end.endsWith('Z') }.index
+            lcm(count, index.toLong())
+        }
+        instructionsNeeded * instructions.length
     }
 }
